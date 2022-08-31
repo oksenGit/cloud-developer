8ca11d3d745d76a401b9e4d4d490d6773eff08bb
Omar <omar.k.elgarhy@gmail.com>
2022-08-31 13:38:56 +0200

    removed TODO and used JWT_SECRET from .env

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -23,7 +23,6 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 }
 
 function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
     return jwt.sign(user.toJSON(), config.jwt.secret);
 }
 
